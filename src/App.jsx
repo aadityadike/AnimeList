712a1a5687235a0df38762b5fce8a04d1b2062ba
aadityadike <adityadike123@gmail.com>
2023-06-01 11:09:51 +0530

    Api link with appshell, Now user can search for anime

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,7 @@
-import { TextField } from '@mui/material'
-import { Button } from '@mui/material'
-import { useEffect, useRef, useState } from 'react'
-import { Api } from './lib/Api'
+import AppShell from "./ui/AppShell";
 
 const App = () => {
-  const valRef = useRef('')
-  const [name, setName] = useState('')
-  const [title, setTitle] = useState('')
-  const [animeData, updateAnimeData] = useState({})
+  return <AppShell />;
+};
 
-  const fetchReq = () => Api
-    .then((res) => res.json().then((json) => res.ok ? json : Promise.reject(json)))
-    .then((data) => updateAnimeData(data))
-    .catch((err) => console.error(err))
-
-  //user input from textfield
-  const userInput = () => {
-    fetchReq()
-    setName(valRef.current.value)
-  }
-
-  // Optional chaining: To avoid errors when accessing nested properties like animeData.data.Media.title.english, you can use optional chaining (?.) to handle cases where the properties are not defined.
-  useEffect(() => {
-    setTitle(animeData?.data?.Media?.title?.english)
-  }, [animeData])
-
-  return (
-    <div className='flex items-center justify-center h-screen gap-2'>
-      <TextField id='outlined-basic' label="Name" variant="outlined" inputRef={valRef} />
-      <Button variant='outlined' size="large" onClick={userInput}>click me</Button>
-      <div>
-        {title}
-      </div>
-    </div>
-
-  )
-}
-
-export default App
+export default App;
--- src/lib/Api.js
+++ src/lib/Api.js
@@ -1,30 +1,33 @@
-let query = `
-query ($name: String){
-    Media(search: $name, popularity_greater:95555){
-        popularity
-        title {
-            english
-        }
+const Api = (name) => {
+    let query = `
+    query ($name: String){
+        Media(search: $name, popularity_greater:95555){
+            popularity
+            title {
+                english
+            }
     }
 }`
 
-const variables = {
-    name: "demon slayer"
+    const variables = {
+        name: name
+    }
+
+    const url = 'https://graphql.anilist.co'
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+            query: query,
+            variables: variables
+        })
+    }
+
+    return fetch(url, options)
 }
 
-const url = 'https://graphql.anilist.co'
-const options = {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-    },
-    body: JSON.stringify({
-        query: query,
-        variables: variables
-    })
-}
-
-
-export const Api = fetch(url, options)
+export default Api
 
