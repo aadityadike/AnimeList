2178abdac33bf58bc6ef60382387b3cb3e7af124
aadityadike <adityadike123@gmail.com>
2023-06-10 16:40:27 +0530

    improvements

--- src/ui/SearchList.jsx
+++ src/ui/SearchList.jsx
@@ -4,35 +4,22 @@ import { Card, Box } from "@mui/material";
 const SearchList = () => {
   const anime = useSelector((state) => state.fetchData.animeData);
 
-  anime.map((a) =>
-    a.Page.media.map((m) =>
-      m.coverImage != null ? console.log(m) : console.log("image not found")
-    )
-  );
-  
   return (
     <div className="text-center">
       <Box className="flex gap-4">
         {anime.map((a) =>
-          a.Page.media.map((m) =>
-            m.title.english != null ? (
-              <Card
-                key={m.id}
-                className="flex flex-col items-center justify-center flex-wrap w-28"
-              >
-                <img src={m.coverImage.medium} alt="not found" />
-                <a href="">{m.title.english}</a>
-              </Card>
-            ) : (
-              <Card
-                key={m.id}
-                className="flex flex-col items-center justify-center flex-wrap"
-              >
-                <img src={m.coverImage.medium} alt="not found" />
-                <a href="">{m.title.romaji}</a>
-              </Card>
-            )
-          )
+          a.Page.media.map((m) => (
+            <Card
+              key={m.id}
+              className="flex flex-col items-center justify-center flex-wrap w-28"
+              onClick={() => {}}
+            >
+              <img src={m.coverImage.medium} alt="not found" />
+              <a>
+                {m.title.english != null ? m.title.english : m.title.romaji}
+              </a>
+            </Card>
+          ))
         )}
       </Box>
     </div>
